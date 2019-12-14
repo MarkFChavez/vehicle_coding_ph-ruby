8371d52b887a2cf19e85c6125336ab526f4fce66
Mark Chavez <markchav3z@gmail.com>
2019-12-14 21:57:58 +0800

    Initial implementation for window hours
    This commit returns a default data if plate no given
    is not coding given the date.

--- lib/vehicle_coding_ph/checker.rb
+++ lib/vehicle_coding_ph/checker.rb
@@ -2,21 +2,35 @@ module VehicleCodingPh
   class Checker
 
     def self.call(plate_no, date = Date.today)
-      return true if weekend?(date)
+      return allowed_anywhere if not coding?(plate_no, date)
 
-      last_digit = plate_no[-1]
+      VehicleCodingPh::AREA_TO_HOUR_MAPPING.reduce({}) do |area, coding_hours|
+      end
+    end
+
+    # def self.weekend?(date)
+    #   date.saturday? || date.sunday?
+    # end
+    # private_class_method :weekend?
+
+    def self.coding?(plate, date)
+      last_digit = plate[-1]
       day_today = Date::DAYNAMES[date.cwday]
 
       mapping = VehicleCodingPh::PLATE_TO_DAY_MAPPING.
         select { |day, digits| day == day_today }
 
-      !mapping[day_today].include?(last_digit) 
+      mapping[day_today].include?(last_digit)
     end
+    private_class_method :coding?
 
-    def self.weekend?(date)
-      date.saturday? || date.sunday?
+    def self.allowed_anywhere
+      {
+        coding: false,
+        areas_allowed: [:anywhere],
+      }
     end
-    private_class_method :weekend?
+    private_class_method :allowed_anywhere
 
   end
 end
--- spec/lib/vehicle_coding_ph/checker_spec.rb
+++ spec/lib/vehicle_coding_ph/checker_spec.rb
@@ -6,61 +6,79 @@ module VehicleCodingPh
     describe ".call" do
       let(:plate) { "ABC321" }
 
-      context "given that today is Monday" do
-        before { Timecop.freeze("Dec 16, 2019") }
-
-        it "is not allowed to drive" do
-          expect(described_class.(plate)).to eq false
-        end
-      end
-
+      # context "given that today is Monday" do
+      #   before { Timecop.freeze("Dec 16, 2019 10:00:00") }
+      #
+      #   it "is not allowed to drive" do
+      #     expected = {
+      #       coding: true,
+      #       allowed_areas: [
+      #         "Marikina",
+      #         "Muntinlupa",
+      #         "Taguig",
+      #         "Pasig",
+      #         "Paranaque",
+      #       ]
+      #     }
+      #
+      #     expect(described_class.(plate)).to eq expected
+      #   end
+      # end
+      #
       context "given that today is Tuesday" do
         let(:tuesday) { Date.parse("Dec 17, 2019") }
 
         it "is allowed to drive" do
-          expect(described_class.(plate, tuesday)).to eq true
+          result = described_class.(plate, tuesday) 
+
+          expected = {
+            coding: false,
+            areas_allowed: [:anywhere],
+          }
+
+          expect(result).to eq expected
         end
       end
-
-      context "given that today is Wednesday" do
-        before { Timecop.freeze("Dec 18, 2019") }
-
-        it "is allowed to drive" do
-          expect(described_class.(plate)).to eq true
-        end
-      end
-
-      context "given that today is Thursday" do
-        before { Timecop.freeze("Dec 19, 2019") }
-
-        it "is allowed to drive" do
-          expect(described_class.(plate)).to eq true
-        end
-      end
-
-      context "given that today is Friday" do
-        before { Timecop.freeze("Dec 20, 2019") }
-
-        it "is allowed to drive" do
-          expect(described_class.(plate)).to eq true
-        end
-      end
-
-      context "given that today is Saturday" do
-        before { Timecop.freeze("Dec 21, 2019") }
-
-        it "is allowed to drive" do
-          expect(described_class.(plate)).to eq true
-        end
-      end
-
-      context "given that today is Sunday" do
-        before { Timecop.freeze("Dec 22, 2019") }
-
-        it "is allowed to drive" do
-          expect(described_class.(plate)).to eq true
-        end
-      end
+      #
+      # context "given that today is Wednesday" do
+      #   before { Timecop.freeze("Dec 18, 2019") }
+      #
+      #   it "is allowed to drive" do
+      #     expect(described_class.(plate)).to eq true
+      #   end
+      # end
+      #
+      # context "given that today is Thursday" do
+      #   before { Timecop.freeze("Dec 19, 2019") }
+      #
+      #   it "is allowed to drive" do
+      #     expect(described_class.(plate)).to eq true
+      #   end
+      # end
+      #
+      # context "given that today is Friday" do
+      #   before { Timecop.freeze("Dec 20, 2019") }
+      #
+      #   it "is allowed to drive" do
+      #     expect(described_class.(plate)).to eq true
+      #   end
+      # end
+      #
+      # context "given that today is Saturday" do
+      #   before { Timecop.freeze("Dec 21, 2019") }
+      #
+      #   it "is allowed to drive" do
+      #     expect(described_class.(plate)).to eq true
+      #   end
+      # end
+      #
+      # context "given that today is Sunday" do
+      #   before { Timecop.freeze("Dec 22, 2019") }
+      #
+      #   it "is allowed to drive" do
+      #     expect(described_class.(plate)).to eq true
+      #   end
+      # end
     end
 
   end
